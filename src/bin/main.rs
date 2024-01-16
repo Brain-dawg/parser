7800a128fd41cc9d052c519be0a0eb6f02bab2bb
talascio <40523890+talascio@users.noreply.github.com>
2024-01-16 11:03:49 -0500

    batch parsing

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -22,7 +22,8 @@ struct JsonDemo {
 
 use std::fs::File;
 use std::io::Write;
-
+use std::io::Read;
+use zip::ZipArchive;
 fn main() -> Result<(), MainError> {
     #[cfg(feature = "better_panic")]
     better_panic::install();
@@ -37,76 +38,61 @@ fn main() -> Result<(), MainError> {
     }
     let path = args[1].clone();
     let all = args.contains(&std::string::String::from("all"));
-    let detailed_summaries = args.contains(&std::string::String::from("detailed_summaries"));
+    // let detailed_summaries = args.contains(&std::string::String::from("detailed_summaries")); //removed
     let file = fs::read(path)?;
-    let demo = Demo::new(&file);
+    // let demo = Demo::new(&file);
+    // Get an iterator over the entries in the current directory
+    let entries = fs::read_dir(".").unwrap();
+    for entry in entries {
+        // Get the path of the entry
+        let path = entry.unwrap().path();
 
-    // Create a new file to write the output
-    let mut output_file = File::create("output.json")?;
+        // Check if the path is a file and has a .zip extension
+        if path.is_file() && path.extension().unwrap_or_default() == "zip" {
+            // Open the file as a ZipArchive
+            let file = fs::File::open(&path).unwrap();
+            let mut archive = ZipArchive::new(file).unwrap();
+            
+            // Loop through each file in the archive
+            for i in 0..archive.len() {
+                // Get the ZipFile by index
+                let mut zip_file = archive.by_index(i).unwrap();
+                
+                // Create a Vec<u8> to store the contents
+                let mut contents = Vec::new();
+                
+                // Read the contents to the Vec<u8>
+                zip_file.read_to_end(&mut contents).unwrap();
+                
+                // Create a .dem file with the same name as the zip file
+                let dem_path = path.with_extension("dem");
+                let mut dem_file = fs::File::create(&dem_path).unwrap();
 
-    if !detailed_summaries {
-        let parser = if all {
-            DemoParser::new_all(demo.get_stream())
-        } else {
-            DemoParser::new(demo.get_stream())
-        };
-        let (header, state) = parser.parse()?;
-        let demo = JsonDemo { header, state };
+                // Write the contents to the .dem file
+                dem_file.write_all(&contents).unwrap();
 
-        // Write the JSON to the file instead of printing it
-        write!(output_file, "{}", serde_json::to_string(&demo)?)?;
-    } else {
-        let parser = DemoParser::new_with_analyser(demo.get_stream(), PlayerSummaryAnalyzer::new());
-        let (header, state) = parser.parse()?;
+                // Create a new JSON file to write the output
+                let mut output_file = File::create(path.with_extension("json"))?;
 
-        // Write the header to the file
-        write!(output_file, "{:?}", header)?;
+                // Parse the demo file
+                let demo_file_contents = fs::read(&dem_path)?;
+                let demo = Demo::new(&demo_file_contents);
 
-        let table_header = "Player                           | Points     | Kills      | Deaths     | Assists    | Destruction | Captures   | Defenses   | Domination | Revenge    | Ubers      | Headshots  | Teleports  | Healing    | Backstabs  | Bonus      | Support    | Damage Dealt";
-        let divider      = "---------------------------------|------------|------------|------------|------------|-------------|------------|------------|------------|------------|------------|------------|------------|------------|------------|------------|------------|-------------";
-
-        // Write the table header and divider to the file
-        write!(output_file, "{}\n{}", table_header, divider)?;
-
-        for (user_id, user_data) in state.users {
-            let player_name = user_data.name;
-            if let Some(s) = state.player_summaries.get(&user_id) {
-                let (color_code_start, color_code_end) = if player_name == header.nick {
-                    ("\x1b[1;42;37m", "\x1b[0m")
+                let parser = if all {
+                    DemoParser::new_all(demo.get_stream())
                 } else {
-                    ("", "")
+                    DemoParser::new(demo.get_stream())
                 };
+                let (header, state) = parser.parse()?;
+                let json_demo = JsonDemo { header, state };
 
-                // Write the player data to the file
-                write!(
-                    output_file,
-                    "{}{:32} | {:10} | {:10} | {:10} | {:10} | {:11} | {:10} | {:10} | {:10} | {:10} | {:10} | {:10} | {:10} | {:10} | {:10} | {:10} | {:10} | {:12}{}\n",
-                    color_code_start,
+                // Write the JSON output to the file
+                write!(output_file, "{}", serde_json::to_string(&json_demo)?)?;
 
-                    player_name,
-                    s.points,
-                    s.kills,
-                    s.deaths,
-                    s.assists,
-                    s.buildings_destroyed,
-                    s.captures,
-                    s.defenses,
-                    s.dominations,
-                    s.revenges,
-                    s.ubercharges,
-                    s.headshots,
-                    s.teleports,
-                    s.healing,
-                    s.backstabs,
-                    s.bonus_points,
-                    s.support,
-                    s.damage_dealt,
-
-                    color_code_end,
-                )?;
+                // File written, delete extracted demo file
+                fs::remove_file(&dem_path)?;
             }
         }
     }
-
     Ok(())
 }
