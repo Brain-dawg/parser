711f228119773414ba515845091b7795b98a9832
talascio <40523890+talascio@users.noreply.github.com>
2024-02-06 11:17:33 -0500

    compress old data

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,15 +1,24 @@
 use std::env;
 use std::fs;
-
+use std::io;
+use std::io::Write;
+use std::io::Read;
+// use std::io::Seek;
+use std::fs::File;
+use zip::write::FileOptions;
+use zip::CompressionMethod;
+// use flate2::Compression;
+use std::collections::HashSet;
+use std::fs::rename;
+use std::fs::remove_file;
+use std::path::PathBuf;
+// use std::thread;
+use std::time::SystemTime;
 use main_error::MainError;
 use serde::{Deserialize, Serialize};
 use tf_demo_parser::demo::header::Header;
 use tf_demo_parser::demo::parser::analyser::MatchState;
-pub use tf_demo_parser::{Demo, DemoParser, Parse, ParseError, ParserState, Stream};
-
-#[cfg(feature = "jemallocator")]
-#[global_allocator]
-static ALLOC: jemallocator::Jemalloc = jemallocator::Jemalloc;
+use tf_demo_parser::{Demo, DemoParser};
 
 #[derive(Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
@@ -27,105 +36,60 @@ struct HeaderWithFilename {
     header: Header,
 }
 
-use std::io::Write;
-use std::io::Read;
-// use std::fs::File;
-use zip::ZipArchive;
-use std::thread;
-use std::time::Duration;
-use std::collections::HashSet;
-// use std::collections::HashMap;
-use std::fs::OpenOptions;
-use std::path::Path;
-
 fn batchparse(processed_files: &mut HashSet<String>) -> Result<(), MainError> {
-    #[cfg(feature = "better_panic")]
-    better_panic::install();
-
-    #[cfg(feature = "trace")]
-    tracing_subscriber::fmt::init();
-
     let args: Vec<_> = env::args().collect();
     let all = args.contains(&std::string::String::from("all"));
-
-    let entries = fs::read_dir(".").unwrap();
-
+    let entries = fs::read_dir(".")?;
     let file_path = "all_demos.json";
-    let file_exists = Path::new(file_path).exists();
+    let mut output_file = File::create(file_path)?;
+    write!(output_file, "[")?;
 
-    let mut output_file = OpenOptions::new()
-        .write(true)
-        .create(true)
-        .append(file_exists)
-        .open(file_path)?;
-
-    if !file_exists {
-        write!(output_file, "[")?;
-    }
-
-    let mut first = true;
-
-    
     for entry in entries {
         let path = match entry {
             Ok(entry) => entry.path(),
             Err(_) => continue,
         };
-    
+
         if path.is_file() && path.extension().unwrap_or_default() == "zip" {
             let file = match fs::File::open(&path) {
                 Ok(file) => file,
-                Err(_) => {
-                    println!("Failed to open file: {:?}", path);
-                    continue;
-                }
+                Err(_) => continue,
             };
-            let mut archive = match ZipArchive::new(file) {
+            let mut archive = match zip::ZipArchive::new(file) {
                 Ok(archive) => archive,
-                Err(_) => {
-                    println!("Failed to read archive: {:?}", path);
+                Err(_) => continue,
+            };
+
+            for i in 0..archive.len() {
+                if path.with_extension("json").exists() {
                     continue;
                 }
-            };
-    
-            for i in 0..archive.len() {
-                if path.with_extension("json").exists() { continue; }
-    
+
                 let mut zip_file = match archive.by_index(i) {
                     Ok(file) => file,
-                    Err(_) => {
-                        println!("Failed to read file from archive: {:?}", path);
-                        continue;
-                    }
+                    Err(_) => continue,
                 };
+
                 let mut contents = Vec::new();
                 if let Err(_) = zip_file.read_to_end(&mut contents) {
-                    println!("Failed to read contents of file from archive: {:?}", path);
                     continue;
                 }
-    
+
                 let dem_path = path.with_extension("dem");
                 let mut dem_file = match fs::File::create(&dem_path) {
                     Ok(file) => file,
-                    Err(_) => {
-                        println!("Failed to create .dem file: {:?}", dem_path);
-                        continue;
-                    }
+                    Err(_) => continue,
                 };
                 if let Err(_) = dem_file.write_all(&contents) {
-                    println!("Failed to write to .dem file: {:?}", dem_path);
                     continue;
                 }
-    
+
                 let demo_file_contents = match fs::read(&dem_path) {
                     Ok(contents) => contents,
-                    Err(_) => {
-                        println!("Failed to read .dem file: {:?}", dem_path);
-                        continue;
-                    }
+                    Err(_) => continue,
                 };
                 let demo = Demo::new(&demo_file_contents);
-    
+
                 let parser = if all {
                     DemoParser::new_all(demo.get_stream())
                 } else {
@@ -133,48 +97,65 @@ fn batchparse(processed_files: &mut HashSet<String>) -> Result<(), MainError> {
                 };
                 let (header, state) = match parser.parse() {
                     Ok((header, state)) => (header, state),
-                    Err(_) => {
-                        println!("Failed to parse demo: {:?}", dem_path);
-                        continue;
-                    }
+                    Err(_) => continue,
                 };
                 let file_stem = path.file_stem().and_then(|s| s.to_str()).unwrap_or("Unknown");
-    
+
                 let json_demo = JsonDemo { header: HeaderWithFilename { filename: file_stem.to_string(), header }, state };
 
                 if !processed_files.contains(&json_demo.header.filename) {
-                    let _ = write!(output_file, "{{\"data\": {}}}", serde_json::to_string(&json_demo).unwrap());
-                    println!("Writing file: {:?}", file_stem.to_string());
-                    if !first {
-                        if let Err(_) = write!(output_file, ",") {
-                            println!("Failed to write to output file");
-                            continue;
-                        }
+                    if processed_files.is_empty() {
+                        write!(output_file, "{}", serde_json::to_string(&json_demo)?)?;
+                    } else {
+                        write!(output_file, ",{}", serde_json::to_string(&json_demo)?)?;
                     }
+                    println!("Writing: {:?}", &json_demo.header.filename)
                 }
-    
+
                 processed_files.insert(json_demo.header.filename);
-    
-                first = false;
-    
-                if let Err(_) = fs::remove_file(&dem_path) {
-                    println!("Failed to remove .dem file: {:?}", dem_path);
+
+                if let Err(_) = remove_file(&dem_path) {
+                    continue;
                 }
             }
-            
         }
-    }    
+    }
 
     write!(output_file, "]")?;
+
+    // Generate timestamp
+    let timestamp = SystemTime::now()
+        .duration_since(SystemTime::UNIX_EPOCH)
+        .unwrap()
+        .as_secs();
+    
+    // Specify the directory where the zip file will be saved
+    let parsed_old_dir = PathBuf::from("parsed_old");
+    fs::create_dir_all(&parsed_old_dir)?; // Create the directory if it doesn't exist
+
+    // Compose the full path for the zip file
+    let zip_filename = parsed_old_dir.join(format!("all_demos_{}.zip", timestamp));
+
+    // let zip_path = Path::new(&zip_filename);
+
+    let options = FileOptions::default().compression_method(CompressionMethod::Deflated);
+
+    let zip_file = fs::File::create(&zip_filename)?;
+    let mut zip_writer = zip::ZipWriter::new(zip_file);
+    zip_writer.start_file("all_demos.json", options)?;
+    
+    let mut all_demos = fs::File::open("all_demos.json")?;
+    io::copy(&mut all_demos, &mut zip_writer)?;
+    zip_writer.finish()?;
+
+    // Overwrite old JSON file with new data
+    rename("all_demos_new.json", "all_demos.json")?;
 
     Ok(())
 }
 
-fn main() -> Result<(), MainError> {   
-
-    let mut processed_files = HashSet::new();  // Declare HashSet outside of the loop
-    loop {
-        batchparse(&mut processed_files)?;
-        thread::sleep(Duration::from_secs(5));
-    }
+fn main() -> Result<(), MainError> {
+    let mut processed_files = HashSet::new();
+    batchparse(&mut processed_files)?;
+    Ok(())
 }
